99ad44f71dbde206b5dcbb1bebae020d5bee36ae
lary <laryenockh@gmail.com>
2022-11-05 14:07:29 +0100

    third commit of the todo list, almost ready for the deployment

--- script.js
+++ script.js
@@ -22,9 +22,23 @@ function add() {
     const completed = document.createElement('img');
     const check = document.createElement('img');
     const line = document.createElement('div');
+    const space = document.createElement('div')
     const trash = document.createElement('img');
-
     var item = document.querySelector('#item');
+    todos.push(item.value);
+    const option =document.createElement('img');
+    localStorage.setItem('todos', JSON.stringify(todos)) ;
+    const storedTodo = localStorage.getItem('todos')?.toString();
+    
+    
+    if (storedTodo) {
+        todos = JSON.parse(storedTodo);
+        console.log(todos);
+    }
+
+    option.className = 'option';
+    space.style.width = '100px';
+    space.style.marginLeft = '50px'
     trash.className = 'trash'; 
     todo.innerText = item.value;  
     line.className = 'line'
@@ -33,25 +47,30 @@ function add() {
     line.appendChild(completed)
     line.appendChild(check);
     line.appendChild(todo);
+    line.appendChild(space);
     line.appendChild(trash);
+    line.appendChild(option);
     list1.appendChild(line);
-    //todos.push(line)
-
-
+    
     var listes = document.querySelectorAll('.line');
-    localStorage.setItem('todos', listes) 
-    //var locaItem =JSON.parse( localStorage.getItem('todos'));
     
     for (const line of listes) {
 
 
-        line.addEventListener('click', (e)=>{
-            console.log(e);
-            hide(check);
-            //show(completed)
-            show(trash)
-            line.style.cssText = `justify-content: space-between;
-            background-color: rgb(246, 251, 174);`
+        option.addEventListener('click', ()=>{
+            hide(option);
+            show(trash);
+            line.className = 'clickedline';
+            trash.addEventListener('click', (e)=>{
+                console.log(e);
+                var parent = e.path[2];
+                parent.removeChild(e.path[1]);
+            });
+
+            /* document.addEventListener('click', ()=>{
+                line.className = 'unclickedline';
+            }); */
+            
         })
 
         check.addEventListener('click', (line)=>{
@@ -60,6 +79,8 @@ function add() {
             var select = line.path[1];
             select.className = 'select';
             var selected = document.querySelectorAll('.select');
+            hide(option);
+            hide(trash);
             hide(check);
             show(completed);
 
@@ -67,21 +88,26 @@ function add() {
             remove.addEventListener('click', ()=>{
                 for (const item of selected) {
                     hide(item);
+                    //list1.removeChild(line)
                 }
-            })
+            });
 
             completed.addEventListener('click', (e)=>{
                 console.log(e);
                 var deselect = e.path[1];
                 deselect.className = 'deselect';
+                show(option);
+                hide(trash);
                 hide(completed);
-                show(check)
-            })
+                show(check);
+            });
 
             
-        })  
+        }); 
     }
 }
+
+
 
 
 document.addEventListener('keydown', (e)=>{
